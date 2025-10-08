cd3793e0c27e4d70339bb37cc6f6366874116033
laith <laithalwani@gmail.com>
2025-10-08 15:43:23 -0400

    changes app name and fixes spelling mistake

--- lib/main.dart
+++ lib/main.dart
@@ -21,37 +21,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Recipie Book',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
       ),
       home: Consumer(
-        builder: (conetxt, ref, child) {
+        builder: (context, ref, child) {
           final AsyncValue<AppUser?> user = ref.watch(authProvider);
+
           return user.when(
             data: (value) {
-              if (value == null) {
-                return const WelcomeScreen();
-              }
+              if (value == null) return const WelcomeScreen();
               return MainLayout(user: value);
             },
-            error: (error, _) => const Text("error loading auth status..."),
-            loading: () => const Text("loading ..."),
+            error: (error, stack) =>
+                Center(child: Text("Error loading auth status: $error")),
+            loading: () => const Center(child: CircularProgressIndicator()),
           );
         },
       ),
