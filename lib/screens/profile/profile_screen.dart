e4657fafe02cf4a1663532c37fb3a8be0e6b2c57
laith <laithalwani@gmail.com>
2025-10-08 15:43:47 -0400

    fixes sigout render issue

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book/main.dart';
 import 'package:recipe_book/models/app_user.dart';
+import 'package:recipe_book/screens/wlecome/welcome.dart';
 import 'package:recipe_book/services/auth_service.dart';
 
-class Profilescreen extends StatelessWidget {
+class Profilescreen extends StatefulWidget {
   const Profilescreen({super.key, required this.user});
 
   final AppUser? user;
 
+  @override
+  State<Profilescreen> createState() => _ProfilescreenState();
+}
+
+class _ProfilescreenState extends State<Profilescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +24,12 @@ class Profilescreen extends StatelessWidget {
           const Icon(Icons.settings),
           IconButton(
             onPressed: () async {
+              final nav = Navigator.of(context);
               await AuthService.signOut();
+              nav.pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => const MyApp()),
+                (route) => false,
+              );
             },
             icon: const Icon(Icons.logout),
           ),
@@ -34,9 +46,8 @@ class Profilescreen extends StatelessWidget {
             const SizedBox(height: 16),
 
             //out put user email
-            Text("Welcome to profile, ${user!.email}"),
+            Text("Welcome to profile, ${widget.user!.email}"),
             const SizedBox(height: 16),
-
           ],
         ),
       ),
