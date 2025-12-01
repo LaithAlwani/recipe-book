6e4bac72557be60b417bcb297da0e3fc6ba2a5a0
Laith <laithalwani@gmail.com>
2025-11-30 21:44:53 -0500

    ui styles changes

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_book/models/app_user.dart';
-import 'package:recipe_book/screens/profile/settings_screen.dart';
-import 'package:recipe_book/services/auth_service.dart';
 
 class Profilescreen extends StatelessWidget {
   const Profilescreen({super.key, required this.user});
@@ -10,26 +8,30 @@ class Profilescreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 60,
-            backgroundColor: Colors.transparent,
-            backgroundImage: user.photoUrl != null && user.photoUrl!.isNotEmpty
-                ? NetworkImage(user.photoUrl!)
-                : const AssetImage('assets/images/avatar_placeholder.png')
-                      as ImageProvider,
-          ),
-          //out put user email
-          Text(user.displayName),
-          Text(user.email),
-          const SizedBox(height: 16),
-        ],
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile"), centerTitle: true),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.transparent,
+              backgroundImage:
+                  user.photoUrl != null && user.photoUrl!.isNotEmpty
+                  ? NetworkImage(user.photoUrl!)
+                  : const AssetImage('assets/images/avatar_placeholder.png')
+                        as ImageProvider,
+            ),
+            //out put user email
+            Text(user.displayName),
+            Text(user.email),
+            const SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/profile/settings_screen.dart
+++ lib/screens/profile/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_book/models/app_user.dart';
+import 'package:recipe_book/screens/profile/profile_screen.dart';
+import 'package:recipe_book/services/auth_service.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key, required this.user});
@@ -8,7 +10,38 @@ class SettingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Settings"), centerTitle: true),
-      body: const Center(child: Text("Settings Page")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Profilescreen(user: user),
+                ),
+              );
+            },
+
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage:
+                  user.photoUrl != null && user.photoUrl!.isNotEmpty
+                  ? NetworkImage(user.photoUrl!)
+                  : const AssetImage('assets/images/avatar_placeholder.png')
+                        as ImageProvider,
+            ),
+          ),
+          IconButton(
+            onPressed: () async {
+              await AuthService.signOut();
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
     );
   }
 }
