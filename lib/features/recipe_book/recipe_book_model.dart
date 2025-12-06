ff1d3084361e19b2222730b2e735bf8048a729a5
Laith <laithalwani@gmail.com>
2025-12-06 13:33:40 -0500

    create recipe book model

---
+++ lib/features/recipe_book/recipe_book_model.dart
@@ -0,0 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:recipe_book/features/recipie/recipe.dart';
+
+class RecipeBook {
+  final String id;
+  final String title;
+  final List<Recipe> recipies;
+  final Timestamp createdAt;
+  final Timestamp updatedAt;
+  final String? thumbnailUrl;
+
+  RecipeBook({
+    required this.id,
+    required this.title,
+    required this.recipies,
+    required this.createdAt,
+    required this.updatedAt,
+    this.thumbnailUrl,
+  });
+
+  factory RecipeBook.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data()!;
+    return RecipeBook(
+      id: snapshot.id,
+      title: data["title"] ?? '',
+      recipies: (data['recipies'] as List<dynamic>? ?? [])
+          .map((doc) => Recipe.fromFirestore(doc, null))
+          .toList(),
+      createdAt: data['created_at'] is Timestamp
+          ? data['created_at']
+          : Timestamp.now(),
+      updatedAt: data['updated_at'] is Timestamp
+          ? data['updated_at']
+          : Timestamp.now(),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'title': title,
+      'recipies': recipies.map((element) => element.toMap()),
+      'thumbnailUrl': thumbnailUrl,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
+    };
+  }
+
+  RecipeBook copyWith({
+    String? title,
+    List<Recipe>? recipies,
+    Timestamp? createdAt,
+    Timestamp? updatedAt,
+    String? thumbnailUrl,
+  }) {
+    return RecipeBook(
+      id: id,
+      title: title ?? this.title,
+      recipies: recipies ?? this.recipies,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.createdAt,
+      thumbnailUrl: thumbnailUrl ?? this.thumbnailUrl,
+    );
+  }
+}
