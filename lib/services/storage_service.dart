4f6635959032b37d92a66959e0180df78ea12bc0
Laith <laithalwani@gmail.com>
2025-12-08 21:12:37 -0500

    changes storage file strcutre and firebase storage rules

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -6,7 +6,7 @@ class StorageService {
   static final ref = FirebaseStorage.instance.ref().child("user_profiles");
 
   static Future<String> uploadImageAndGetUrl(File file, String uid) async {
-    final storageRef = ref.child(
+    final storageRef = ref.child(uid).child(
       "${DateTime.now().millisecondsSinceEpoch}-$uid.jpg",
     );
     await storageRef.putFile(file);
