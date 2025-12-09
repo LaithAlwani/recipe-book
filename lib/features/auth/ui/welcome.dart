9c7b44aeced1f3c6d4dc7d5e863fb19e17a344ed
Laith <laithalwani@gmail.com>
2025-12-09 13:04:13 -0500

    removes unsed import

--- lib/features/auth/ui/welcome.dart
+++ lib/features/auth/ui/welcome.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:recipe_book/features/auth/auth_provider.dart';
-import 'package:recipe_book/features/auth/auth_state.dart';
 import 'package:recipe_book/features/auth/ui/signin.dart';
 import 'package:recipe_book/features/auth/ui/signup.dart';
 import 'package:recipe_book/shared/social_logins.dart';
